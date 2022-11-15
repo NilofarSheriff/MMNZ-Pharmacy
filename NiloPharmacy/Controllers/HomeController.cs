ef26662263a50a254d21028bf033fb8ba9492a26
Nilofar <nilofarsheriff@gmail.com>
2022-11-15 10:04:00 +0530

    About us done

---
+++ NiloPharmacy/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NiloPharmacy.Data.ViewModels;
+using System.Net.Mail;
+
+namespace NiloPharmacy.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(ContactUsViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage msz = new MailMessage();
+                    msz.From = new MailAddress(vm.Email);//Email which you are getting 
+                                                         //from contact us page 
+                    msz.To.Add("Pharmacy@gmail.com");//Where mail will be sent 
+                    msz.Subject = vm.Subject;
+                    msz.Body = vm.Message;
+                    SmtpClient smtp = new SmtpClient();
+
+                    smtp.Host = "smtp.gmail.com";
+
+                    smtp.Port = 587;
+
+                    smtp.Credentials = new System.Net.NetworkCredential
+                    ("youremailid@gmail.com", "password");
+
+                    smtp.EnableSsl = true;
+
+                    smtp.Send(msz);
+
+                    ModelState.Clear();
+                    ViewBag.Message = "Thank you for Contacting us ";
+                }
+                catch (Exception ex)
+                {
+                    ModelState.Clear();
+                    ViewBag.Message = $" Sorry we are facing Problem here {ex.Message}";
+                }
+            }
+
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
